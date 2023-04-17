becd27929128711b29f2dd8ca0e2865e035cf825
Ajitesh Singh <ajitesh1802.be20@chitkara.edu.in>
2023-04-17 10:57:29 +0530

    searchbar updated

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const User = require("./models/user")
+const mongoose = require("mongoose")
+
 require('./db/conn')
 const app = express();
 var inputs = [];
@@ -9,8 +11,10 @@ var inputs = [];
  
 app.set('view engine','ejs');
 
+app.use(express.json());
+
 app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:false}))
 
 
 
@@ -19,26 +23,90 @@ app.use(bodyParser.urlencoded({extended:true}))
         res.render("main")
     })
 
-app.post('/client',async(req,res)=>{
+    
+
+app.post('/',async(req,res)=>{
     try{
         //  res.send(req.body)
-        const userData = User(req.body);
+        const userData = new User({
+            clientName:req.body.clientName,
+            propertyChoice:req.body.propertyChoice,
+            location:req.body.location,
+            budget:req.body.budget,
+            phone:req.body.phone,
+            email:req.body.email,
+            id:req.body.id,
+     });
+
+       
+     if(userData.location=="Chandigarh"){
        await userData.save();
+     }
+     
         res.status(201).render("main");
-        }
-        catch(error){
-            res.status(500).send(error)
-        }
-  
+     
+    }         catch(error){
+        res.status(500).send(error)
+    }
 
-})
-console.log(User.find({}));
-// app.get('/fetch',(req,res)=>{
-
-//   })
+//     User.find({}).then((err,foundItems)=>{
+//         console.log(foundItems);
 // })
 
-// 11e7f180282b7d8a5e4377f1806c1c94-us17
+// console.log(User.find());
+// console.log(User.findById({id:fetchid}));
+// console.log(User.find({}));
+
+})
+
+
+app.get("/client17",(req,res)=>{
+    //     fetchid = req.params.id;
+    //   console.log(User.findById({id:fetchid}));
+    //   res.send(User.find({}));
+ 
+
+  
+    User.find().then((result)=>{
+        
+        // result.forEach((data)=>{
+           result.reverse();
+            res.render("crm",{kindOfDay:"Monday",record:result});
+            
+            // res.send(data.clientName + " ");
+        // })
+    }).catch((err)=>{
+        console.log(err);
+    })
+     })
+
+     app.post("/client17/search/",(req,res)=>{
+
+        let value = req.body.value;
+        let data = [];
+        User.find({$or: [
+            {location: {$regex: new RegExp(value, "i")} },
+           {clientName: {$regex: new RegExp(value, "i")}},
+           {phone: {$regex: new RegExp(value, "i")}}
+
+        ]}).then((result)=>{
+        
+            // result.forEach((data)=>{
+                console.log(result);
+               result.reverse();
+               res.render("crm",{kindOfDay:"Monday",record:result});
+               // res.send(data.clientName + " ");
+               // })
+            }).catch((err)=>{
+                console.log(err);
+            })
+
+     })
+
+
+// app.get("/crm",(req,res)=>{
+
+// })
 
 app.listen(5000,(req,res)=>{
     console.log(`Server Is Running On 5000`);
--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     id:{
-        type:String
+        type:Number
     },
     clientName:{
         type:String,
